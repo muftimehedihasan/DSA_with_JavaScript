8026afd01338a645d9efeb289ae5e59397ace283
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-09-09 23:43:01 +0600

    Prototype in javascript

---
+++ Prototype_in_javascript.js
@@ -0,0 +1,39 @@
+// // Constructor function for creating a car object
+// function Car(make, model) {
+//     this.make = make;
+//     this.model = model;
+//   }
+  
+//   // Adding a method to the Car's prototype
+//   Car.prototype.startEngine = function() {
+//     return 'Engine started';
+//   };
+  
+//   // Creating a new instance of Car
+//   let myCar = new Car('Toyota', 'Camry');
+  
+//   // Accessing the startEngine method from the prototype
+//   console.log(myCar.startEngine());  // Outputs: 'Engine started'
+  
+//   // Displaying the newly created car object
+//   console.log(myCar);  // Outputs the car object with make and model
+
+
+// function Animal(Name, IsMammal, HasTail){
+//     this.Name = name;
+//     this.IsMammal = IsMammal;
+//     this.HasTail = HasTail;
+// }
+
+// // This part is for Logging Prototype on console
+// console.log(Animal.prototype);
+
+// https://bn.quora.com/%E0%A6%9C%E0%A6%BE%E0%A6%AD%E0%A6%BE%E0%A6%B8%E0%A7%8D%E0%A6%95%E0%A7%8D%E0%A6%B0%E0%A6%BF%E0%A6%AA%E0%A7%8D%E0%A6%9F-%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A7%8B%E0%A6%9F%E0%A7%8B%E0%A6%9F%E0%A6%BE%E0%A6%87%E0%A6%AA
+
+// let myName = {
+//     name: "Mehedi",
+//     age: 29,
+
+// }
+
+// console.log(myName);
