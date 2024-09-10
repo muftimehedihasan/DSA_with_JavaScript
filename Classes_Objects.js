b4c8fbdd21a715faf9cd9f156fa10424ed6bb849
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-09-11 00:29:27 +0600

    new commit

---
+++ Classes_Objects.js
@@ -0,0 +1,22 @@
+// const employee = {
+//     calcTax() {
+//         console.log("tax rate is 10%");
+//     },
+// };
+
+// const karanArjun = {
+//     slay: 50000,
+//     calcTax() {
+//         console.log("tax rate is 20%");
+//     },
+// };
+
+// karanArjun._proto_ = employee;
+
+const student = {
+    fullName: "shradha khapra",
+    marks: 94.4,
+    printMarks: function () {
+        console.log("marks = ", this.marks);
+    },
+};
